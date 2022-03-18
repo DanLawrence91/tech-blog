5c44f9df19d2d213a0506ee901b2ad263f893c8e
Daniel Lawrence <dan.lawrence0810@gmail.com>
2022-03-18 18:33:38 +1030

    corrected get day function, and amended layouts on on views section

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,6 @@
 module.exports = {
   format_date: (date) => {
-    const day = date.getDay();
+    const day = date.getDate();
     const month = date.getMonth() + 1;
     const year = date.getFullYear();
 
