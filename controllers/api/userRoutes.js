d311c480ef2c391de188da145f7ff27e75907dba
Daniel Lawrence <dan.lawrence0810@gmail.com>
2022-03-22 16:40:27 +1030

    clean up users

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,16 @@
 const router = require("express").Router();
 const { User } = require("../../models");
 
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.findAll();
+
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 // creats a new user on sign up page
 router.post("/", async (req, res) => {
   try {
